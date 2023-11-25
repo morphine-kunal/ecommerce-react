3faf3e693301733e00c24a029c1b47fdf83fbe52
Kunal Kishore <kunalkishore1807@gmail.com>
2023-11-25 14:10:04 +0530

    feat: Added react router and react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import HomePage from "./Pages/homePage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="bg-[#F6F6F6]">
-      <HomePage />
+      <Routes>
+        <Route path="/" exact Component={HomePage} />
+      </Routes>
     </div>
   );
 }
