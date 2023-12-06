dff923b10c984242976ed9b098efd678c2e923fa
Kunal Kishore <kunalkishore1807@gmail.com>
2023-12-06 16:51:52 +0530

    feat: product details page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import HomePage from "./Pages/homePage";
 import { Route, Routes } from "react-router-dom";
+import ProductDetails from "./components/Products/productDetails";
 
 function App() {
   return (
     <div className="bg-[#F6F6F6]">
       <Routes>
         <Route path="/" exact Component={HomePage} />
+        <Route path="/product/:id" Component={ProductDetails} />
       </Routes>
     </div>
   );
