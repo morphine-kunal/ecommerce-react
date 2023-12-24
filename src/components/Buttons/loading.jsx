ee0ec192301597733956cc8b0a8f27a1dc9c3985
Kunal Kishore <kunalkishore1807@gmail.com>
2023-12-24 19:28:43 +0530

    feat: loading spinner created

--- src/components/Buttons/loading.jsx
+++ src/components/Buttons/loading.jsx
@@ -4,7 +4,7 @@ const Loading = () => {
     height: "100px",
     borderRadius: "50%",
     border: "2px solid #ddd",
-    borderTop: "2px solid black",
+    borderTop: "2px solid #ff003c",
   };
   return <div className="animate-spin topLeft" style={myStyle}></div>;
 };
