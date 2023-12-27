fa793204886f7cd38069525de05b85ed7349dc46
Kunal Kishore <kunalkishore1807@gmail.com>
2023-12-27 19:14:21 +0530

    logo click handler added

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,11 +1,23 @@
 // import React from 'react'
+import { useNavigate } from "react-router-dom";
 import logo from "../../assets/ZzzzAP.svg";
 const Header = () => {
+  const history = useNavigate();
+
+  const clickHandler = (e) => {
+    e.preventDefault();
+    history("/");
+  };
   return (
     <header className="border-b-2  bg-[#FFFFFF]">
       <div className="w-[90%] mx-auto h-14 flex justify-between items-center px-3">
         <div className="w-24 h-auto">
-          <img src={logo} alt="logo" className="w-full h-full" />
+          <img
+            src={logo}
+            alt="logo"
+            className="w-full h-full cursor-pointer"
+            onClick={clickHandler}
+          />
         </div>
         <p className="p-2 bg-red-500 text-white font-bold">
           The project is under construction
