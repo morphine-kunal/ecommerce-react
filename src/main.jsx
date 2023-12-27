97ab1b8a2d6b2a49e034c08374ebff77499978dc
Kunal Kishore <kunalkishore1807@gmail.com>
2023-12-27 18:55:52 +0530

    main .jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/subdirectory">
       <App />
     </Router>
   </React.StrictMode>
