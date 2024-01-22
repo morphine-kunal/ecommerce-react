9d83e10eb31dde33585d81154bbd44dd4acfea15
Kunal Kishore <kunalkishore1807@gmail.com>
2024-01-23 01:42:02 +0530

    feat: productdescription style modified

--- src/components/Products/productDescription.jsx
+++ src/components/Products/productDescription.jsx
@@ -3,30 +3,44 @@ import { useState } from "react";
 
 import TabBtn from "../Buttons/tabBtn";
 
-const ProductDescription = ({description}) => {
-  const [showFeatures, setShowFeatures] = useState(false);
+const ProductDescription = ({ description }) => {
+  const [showFeatures, setShowFeatures] = useState(true);
   const [showDescription, setShowDescription] = useState(false);
+  const [isFeatureClicked, setIsFeatureClick] = useState(true);
+  const [isDescriptionClicked, setIsDescriptionClicked] = useState(false);
 
   const handleFeatureClick = () => {
     setShowFeatures(!showFeatures);
     setShowDescription(false);
+    setIsFeatureClick(!isFeatureClicked);
+    setIsDescriptionClicked(false);
   };
   const handleDescriptionClick = () => {
     setShowFeatures(false);
     setShowDescription(!showDescription);
+    setIsDescriptionClicked(!isDescriptionClicked);
+    setIsFeatureClick(false);
   };
 
   return (
     <div className="mt-3">
       <div className="w-56 flex justify-between items-center">
-        <TabBtn name="Features" click={handleFeatureClick} />
-        <TabBtn name="Description" click={handleDescriptionClick} />
+        <TabBtn
+          name="Features"
+          click={handleFeatureClick}
+          bgc={isFeatureClicked ? "bg-black text-white" : ""}
+        />
+        <TabBtn
+          name="Description"
+          click={handleDescriptionClick}
+          bgc={isDescriptionClicked ? "bg-black text-white" : ""}
+        />
       </div>
 
       {showFeatures && (
         <div className="bg-white p-5 mt-3 rounded-md">
           <p className="text-lg mb-2 font-semibold">Features</p>
-          <div>
+          <div className="text-xs md:text-base">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
             repellendus iure nihil ea corrupti aliquid expedita delectus placeat
             illo minus praesentium facilis voluptas rerum, veritatis sequi optio
@@ -45,8 +59,19 @@ const ProductDescription = ({description}) => {
           <p className="text-lg mb-2 font-semibold">Description</p>
 
           <p className="italic text-sm mb-2 font-semibold">{description}</p>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum reprehenderit, consectetur possimus dicta quibusdam nam fuga eos facere ab quod consequatur dolor tenetur omnis autem quaerat aut culpa nihil quis unde impedit. Quo sint ipsum harum velit impedit dolorem delectus hic maxime saepe, ipsam, non itaque commodi, totam eos eaque? Deserunt cumque nisi ducimus corporis quis, velit voluptates molestiae quod quidem, alias maxime distinctio at pariatur omnis, illo inventore culpa. Sapiente est commodi eos animi labore consequuntur, impedit accusantium, consectetur eligendi numquam at non molestias nostrum odio! Ad quibusdam blanditiis maiores quo veniam? Dicta voluptatum quibusdam hic sunt? Necessitatibus, possimus?
+          <div className="text-xs md:text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            reprehenderit, consectetur possimus dicta quibusdam nam fuga eos
+            facere ab quod consequatur dolor tenetur omnis autem quaerat aut
+            culpa nihil quis unde impedit. Quo sint ipsum harum velit impedit
+            dolorem delectus hic maxime saepe, ipsam, non itaque commodi, totam
+            eos eaque? Deserunt cumque nisi ducimus corporis quis, velit
+            voluptates molestiae quod quidem, alias maxime distinctio at
+            pariatur omnis, illo inventore culpa. Sapiente est commodi eos animi
+            labore consequuntur, impedit accusantium, consectetur eligendi
+            numquam at non molestias nostrum odio! Ad quibusdam blanditiis
+            maiores quo veniam? Dicta voluptatum quibusdam hic sunt?
+            Necessitatibus, possimus?
           </div>
         </div>
       )}
