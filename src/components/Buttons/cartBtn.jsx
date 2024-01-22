d9d7ad29a0ad324b534fc3fa938d6b44f335dd48
Kunal Kishore <kunalkishore1807@gmail.com>
2024-01-23 01:39:49 +0530

    feat: add cartBtn made working

--- src/components/Buttons/cartBtn.jsx
+++ src/components/Buttons/cartBtn.jsx
@@ -1,9 +1,13 @@
 // import React from 'react'
 import { IoCartOutline } from "react-icons/io5";
 
-const CartBtn = () => {
+const CartBtn = (props) => {
   return (
-    <button className="bg-[#ff003c] px-4 py-2 w-full flex items-center rounded-md text-white justify-center cursor-pointer">
+    <button
+      className="bg-[#ff003c] px-4 py-2 w-full flex items-center rounded-md text-white justify-center cursor-pointer"
+      // eslint-disable-next-line react/prop-types
+      onClick={props.addToCart}
+    >
       <span className="w-5 h-5 mr-2">
         <IoCartOutline className="w-5 h-5" />
       </span>
--- src/components/Buttons/inTheBasket.jsx
+++ src/components/Buttons/inTheBasket.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/prop-types */
 // import React from 'react'
 import verify from "../../assets/basket.png";
 
 const InTheBasket = (props) => {
     
   return (
-    <button className="bg-gray-200 px-5 py-2 flex justify-center items-center text-sm rounded-md">
+    <button className="bg-gray-200 px-5 py-2 flex justify-center items-center text-sm rounded-md" onClick={props.onclick}>
       <span className="w-5 h-5 mr-2">
         <img src={verify} alt="verify" className="w-5 h-5" />
       </span>
